eb0839a0c05aa882063c750fdff13ff501a04ee8
sophia <sophiaoliveira2706@outlook.com>
2026-06-22 14:03:49 -0300

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from '../../imgs/plump-preto.png';
+
+export const Header = () => {
+  return (
+    <header className='font-sans border-b '>
+        <article className='text-center bg-charcoal text-off-white uppercase text-xs p-2 tracking-widest'>Frete grátis acima de R$ 99 · Amostra grátis em todo pedido</article>
+        <nav className='container flex justify-between items-center p-1'>
+            <div>
+                <img src={logo} alt="" className='w-45 cursor-pointer pb-2' />
+            </div>
+
+            <ul className='flex gap-5 items-center [&>*:not(:last-child):hover]:underline'>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Formulação</a></li>
+                <li><a href="">Como Usar</a></li>
+                <li><a href="">Comprar</a></li>
+                <li className='border py-2 px-3 hover:bg-charcoal hover:text-off-white hover:transition'><a href="">Carrinho</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
---
+++ src/components/header/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <section className='container py-2.5'>
+        <span className='uppercase text-peach-dark text-sm  block text-center'>Extrait de Pêche · 10% · Hydrating + Glow</span>
+        <h1 className='text-9xl text-center py-3 font-bold '>Hidratação com brilho <span className='text-peach'>de verdade.</span></h1>
+    </section>
+  )
+}
